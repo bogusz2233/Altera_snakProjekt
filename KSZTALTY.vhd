05a756b0be9eeac5e61310ed5e9985c05a4bde05
Łukasz Boguszewski <bogusz2233@gmail.com>
2017-04-06 15:55:06 +0200

    added circle in procedure, that just draw a circle on a screen

--- KSZTALTY.vhd
+++ KSZTALTY.vhd
@@ -3,7 +3,13 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 PACKAGE KSZTALTY IS
-procedure PROSTOKAT (VARIABLE SIZE_X, SIZE_Y,POS_X,POS_Y :integer; SIGNAL CUR_X,CUR_Y :IN INTEGER; signal DRAW_RECT :out std_logic);
+procedure PROSTOKAT (VARIABLE SIZE_X, SIZE_Y, POS_X, POS_Y :integer; 
+							SIGNAL CUR_X,CUR_Y :IN INTEGER; 
+							signal DRAW_RECT :out std_logic);
+							
+procedure CIRCLE (VARIABLE SIZE, POS_X,POS_Y :integer; 
+							SIGNAL CUR_X,CUR_Y :IN INTEGER; 
+							signal DRAW_CIRCLE :out std_logic);
 
 END KSZTALTY;
 
@@ -14,14 +20,33 @@ PACKAGE BODY KSZTALTY IS
 	--POS_X , POS_Y OBECNA POZYCJA ELEMENTU
 	-- DRAW_RECT SYGNAL POTWIERDZAJACY RYSOWANIE ELEMENTU
 	
-	PROCEDURE PROSTOKAT (VARIABLE SIZE_X, SIZE_Y,POS_X,POS_Y :integer; SIGNAL CUR_X,CUR_Y :IN INTEGER; signal DRAW_RECT :out std_logic) IS
+	PROCEDURE PROSTOKAT (VARIABLE SIZE_X, SIZE_Y,POS_X,POS_Y :integer;
+								SIGNAL CUR_X,CUR_Y :IN INTEGER; 
+								signal DRAW_RECT :out std_logic) IS
 		BEGIN
 			IF(CUR_X >= POS_X AND CUR_X < POS_X + SIZE_X) AND (CUR_Y >= POS_Y AND CUR_Y < POS_Y + SIZE_Y) THEN
-				DRAW_RECT<='1';
+				DRAW_RECT <='1';
 			ELSE
-				DRAW_RECT<='0';
+				DRAW_RECT <='0';
 			END IF;
 	
 	END PROSTOKAT;
+	
+	procedure CIRCLE (VARIABLE SIZE, POS_X,POS_Y :integer; 
+							SIGNAL CUR_X,CUR_Y :IN INTEGER; 
+							signal DRAW_CIRCLE :out std_logic) IS
+		
+		VARIABLE SUMA_X,SUMA_Y		:INTEGER;
+		BEGIN
+			SUMA_X := (CUR_X - POS_X - SIZE) * (CUR_X - POS_X - SIZE);
+			SUMA_Y := (CUR_Y - POS_Y - SIZE) * (CUR_Y - POS_Y - SIZE);
+			
+			IF (( SUMA_X + SUMA_Y)<= (SIZE * SIZE)) THEN 
+				DRAW_CIRCLE <= '1';
+			ELSE 
+				DRAW_CIRCLE <= '0';
+			END IF;
+		
+	END CIRCLE;
 
 END KSZTALTY;
